c6fcb653af4ed15942b53ec516e1c30834e67dd8
OAK-02 <omarbdllh1@gmail.com>
2025-08-05 19:54:12 +1000

    my working on verfication

--- verkle.py
+++ verkle.py
@@ -58,30 +58,11 @@ def hash(x):
             b += hash(a.compress())
     return hash(b)
 
-
 # Commitment is 256 bits
 
 # websites
 # https://lucasmartincalderon.medium.com/verkle-trees-everything-you-need-to-know-321c9c8bc2f6
 # https://blog.ethereum.org/2021/12/02/verkle-tree-structure
-
-## A verkle tree is a Merkle tree with polynomial commitments at each node.
-"""
-    Need the following constructs:
-    - Verkle tree data structure
-    - Prove and verify functions
-"""
-
-
-class NodeType(Enum):
-    # Every node is either
-    # (i) empty,
-    # (ii) a leaf node containing a key and value, or
-    # (iii) an intermediate node that has some fixed number of children
-    # (the "width" of the tree)
-    EMPTY = 0
-    INNER = 1
-    LEAF = 2
 
 
 class VerkleTree:
@@ -95,6 +76,7 @@ class VerkleTree:
     branch_factor = -1
     ROOT_OF_UNITY = pow(PRIMITIVE_ROOT, (MODULUS - 1) // WIDTH, MODULUS)
     DOMAIN = []
+    
 
     # Empty || Leaf(Key, Value) || Node(Commitment, Children)
     def __init__(self, branch_factor: int):
@@ -130,7 +112,6 @@ class VerkleTree:
     """
     Update or insert node and update all commitments and hashes
     """
-
     def insert_update_node(self, key: bytes, value: bytes):
         node = self.root
         path = self.get_verkle_indices(key)
@@ -208,32 +189,178 @@ class VerkleTree:
 
     def root_commit(self):
         return self.root.commitment
+    
+    def check_kzg_multiproof(self, Cs, indices, ys, proof, display_times=True):
+        """
+        Verifies a KZG multiproof according to the schema described here:
+        https://dankradfeist.de/ethereum/2021/06/18/pcs-multiproofs.html
+        """
+        D_serialized, y, sigma_serialized = proof
+        D = blst.P1(D_serialized)
+        sigma = blst.P1(sigma_serialized)
 
-    def prove(self, key: int):
-        path = self._key_path(key)
-        node = self.root
-        proof = []
-        for idx in path:
-            # build node's polynomial
-            poly = []
-            for child in node.children:
-                if child is None:
-                    poly.append(0)
-                elif child.children is None:
-                    poly.append(child.value % curve.curve_order)
-                else:
-                    poly.append(hash_point_to_field(child.commitment))
-            # open at index idx
-            v, pi = open_poly(poly, idx, self.srs)
-            proof.append({"C": node.commitment, "i": idx, "v": v, "pi": pi})
-            # descend
-            nxt = node.children[idx]
-            if nxt is None:
-                raise KeyError(f"No entry for key {key}")
-            if nxt.children is None:
-                break
-            node = nxt
-        return proof
+        # Step 1
+        r = hash_to_int([hash(C) for C in Cs] + ys + [kzg_utils.DOMAIN[i] for i in indices]) % MODULUS
+
+        # log_time_if_eligible("   Computed r hash", 30, display_times)
+        
+        # Step 2
+        t = hash_to_int([r, D])
+        E_coefficients = []
+        g_2_of_t = 0
+        power_of_r = 1
+
+        for index, y in zip(indices, ys):
+            E_coefficient = primefield.div(power_of_r, t - DOMAIN[index])
+            E_coefficients.append(E_coefficient)
+            g_2_of_t += E_coefficient * y % MODULUS
+                
+            power_of_r = power_of_r * r % MODULUS
+
+        # log_time_if_eligible("   Computed g2 and e coeffs", 30, display_times)
+        
+        E = pippenger.pippenger_simple(Cs, E_coefficients)
+
+        # log_time_if_eligible("   Computed E commitment", 30, display_times)
+
+        # Step 3 (Check KZG proofs)
+        w = (y - g_2_of_t) % MODULUS
+
+        q = hash_to_int([E, D, y, w])
+
+        if not kzg_utils.check_kzg_proof(E.dup().add(D.dup().mult(q)), t, y + q * w, sigma):
+            return False
+
+        # log_time_if_eligible("   Checked KZG proofs", 30, display_times)
+
+        return True
+    
+    def make_kzg_multiproof(self, Cs, fs, indices, ys, display_times=True):
+        """
+        Computes a KZG multiproof according to the schema described here:
+        https://dankradfeist.de/ethereum/2021/06/18/pcs-multiproofs.html
+
+        zs[i] = DOMAIN[indexes[i]]
+        """
+
+        # Step 1: Construct g(X) polynomial in evaluation form
+        r = hash_to_int([hash(C) for C in Cs] + ys + [kzg_utils.DOMAIN[i] for i in indices]) % MODULUS
+
+        # log_time_if_eligible("   Hashed to r", 30, display_times)
+
+        g = [0 for i in range(WIDTH)]
+        power_of_r = 1
+        for f, index in zip(fs, indices):
+            quotient = kzg_utils.compute_inner_quotient_in_evaluation_form(f, index)
+            for i in range(WIDTH):
+                g[i] += power_of_r * quotient[i]
+
+            power_of_r = power_of_r * r % MODULUS
+
+        # log_time_if_eligible("   Computed g polynomial", 30, display_times)
+
+        D = kzg_utils.compute_commitment_lagrange({i: v for i, v in enumerate(g)})
+
+        # log_time_if_eligible("   Computed commitment D", 30, display_times)
+
+        # Step 2: Compute h in evaluation form
+        
+        t = hash_to_int([r, D]) % MODULUS
+        
+        h = [0 for _ in range(WIDTH)]
+        power_of_r = 1
+        
+        for f, index in zip(fs, indices):
+            denominator_inv = primefield.inv(t - DOMAIN[index])
+            for i in range(WIDTH):
+                h[i] += power_of_r * f[i] * denominator_inv % MODULUS
+                
+            power_of_r = power_of_r * r % MODULUS
+    
+        # log_time_if_eligible("   Computed h polynomial", 30, display_times)
+
+        # Step 3: Evaluate and compute KZG proofs
+
+        y, pi = kzg_utils.evaluate_and_compute_kzg_proof(h, t)
+        w, rho = kzg_utils.evaluate_and_compute_kzg_proof(g, t)
+
+
+        # Compress both proofs into one
+
+        E = kzg_utils.compute_commitment_lagrange({i: v for i, v in enumerate(h)})
+        q = hash_to_int([E, D, y, w])
+        sigma = pi.dup().add(rho.dup().mult(q))
+
+        # log_time_if_eligible("   Computed KZG proofs", 30, display_times)
+
+        return D.compress(), y, sigma.compress()
+
+    def find_node_with_path(self, root, key):
+        """
+        As 'find_node', but returns the path of all nodes on the way to 'key' as well as their index
+        """
+        current_node = root
+        indices = iter(self.get_verkle_indices(key))
+        path = []
+        current_index_path = []
+        while current_node["node_type"] == "inner":
+            index = next(indices)
+            path.append((tuple(current_index_path), index, current_node))
+            current_index_path.append(index)
+            if index in current_node:
+                current_node = current_node[index]
+            else:
+                return path, None
+        if current_node["key"] == key:
+            return path, current_node
+        return path, None
+
+    def make_verkle_proof(self, tree: "VerkleTree", keys: bytes, display_times=True):
+        """
+        Creates a proof for the 'keys' in the verkle tree given by 'tree'
+        """
+        # Step 0: Find all keys in the trie
+        nodes_by_index = {}
+        nodes_by_index_and_subindex = {}
+        values = []
+        depths = []
+        for key in keys:
+            path, node = self.find_node_with_path(tree, key)
+            depths.append(len(path))
+            values.append(node.value if node.node_type == NodeType.LEAF else None)
+            for index, subindex, node in path:
+                nodes_by_index[index] = node
+                nodes_by_index_and_subindex[(index, subindex)] = node
+
+        # log_time_if_eligible("   Computed key paths", 30, display_times)
+        
+        # All commitments, but without any duplications. These are for sending over the wire as part of the proof
+        nodes_sorted_by_index = list(map(lambda x: x[1], sorted(nodes_by_index.items())))
+        
+        # Nodes sorted 
+        nodes_sorted_by_index_and_subindex = list(map(lambda x: x[1], sorted(nodes_by_index_and_subindex.items())))
+        
+        indices = list(map(lambda x: x[0][1], sorted(nodes_by_index_and_subindex.items())))
+        
+        ys = list(map(
+            lambda x: int.from_bytes((x[1][x[0][1]]).hash, "little"), 
+            sorted(nodes_by_index_and_subindex.items())
+        ))
+        
+        # log_time_if_eligible("   Sorted all commitments", 30, display_times)
+
+        fs = []
+        Cs = [x for x in nodes_sorted_by_index_and_subindex]
+
+        for node in nodes_sorted_by_index_and_subindex:
+            fs.append([int.from_bytes(node[i].hash, "little") if i in node else 0 for i in range(WIDTH)])
+
+        D, y, sigma = self.make_kzg_multiproof(Cs, fs, indices, ys, display_times)
+
+        commitments_sorted_by_index_serialized = [x.commitment.compress() for x in nodes_sorted_by_index[1:]]
+        
+        # log_time_if_eligible("   Serialized commitments", 30, display_times)
+        return depths, commitments_sorted_by_index_serialized, D, y, sigma
 
     @staticmethod
     # TODO: Joules
@@ -241,6 +368,21 @@ class VerkleTree:
     def verify(self, key: int, value: int, proof):
         pass
 
+## A verkle tree is a Merkle tree with polynomial commitments at each node.
+"""
+    Need the following constructs:
+    - Verkle tree data structure
+    - Prove and verify functions
+"""
+class NodeType(Enum):
+    # Every node is either
+    # (i) empty,
+    # (ii) a leaf node containing a key and value, or
+    # (iii) an intermediate node that has some fixed number of children
+    # (the "width" of the tree)
+    EMPTY = 0
+    INNER = 1
+    LEAF = 2
 
 # Use KZG settings as seen in verkle_trie
 class VerkleNode:
@@ -272,9 +414,7 @@ class VerkleNode:
         self.value = value
         self.children = None if (value is not None) else [None] * branch_factor
         self.node_type = node_type
-        # Omar you fucking retard, set the commitment to the identity element
         self.commitment = blst.G1().mult(0)
-
 
 # KZG Commitment (https://raw.githubusercontent.com/giuliop/plonk/main/kzg.py)
 # Curve order
@@ -347,6 +487,8 @@ def add_node_hash(node: VerkleNode):
         node.commitment = commitment
         node.hash = hash(commitment.compress())
 
+def hash_to_int(data):
+    return int.from_bytes(hash(data), "little")
 
 def generate_setup(s):
     pass
